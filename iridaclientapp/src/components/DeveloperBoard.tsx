d2b89a8089303e980d1a5d9fc6882ee95fe1fb9a
magak <magakk.stepanyan@gmail.com>
2020-06-18 19:46:46 +0300

    developer board

--- iridaclientapp/src/components/DeveloperBoard.tsx
+++ iridaclientapp/src/components/DeveloperBoard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import Box from '@material-ui/core/Box';
 import { TextField, Button } from '@material-ui/core';
 
@@ -9,30 +10,32 @@ const useStyles = makeStyles((theme: Theme) =>
         height: '100%',
         position: 'relative',
         overflow: 'auto',
+        border: '2px solid',
+        borderRadius: 10,
+        borderColor: theme.palette.secondary.main,
 
     },
     mainBox: {
         position: 'absolute',
         display: 'flex',
-        flexDirection: 'column',
-        border: 2,
-        borderRadius: 10,
-        borderColor: "secondary.main"
+        height: '90%',
+        width: '90%',
+        flexDirection: (wideScrenMode: Boolean) => wideScrenMode ? "column" : "row"
     },
     button: {
         margin: theme.spacing(2)
     },
     textField : {
-        margin: theme.spacing(3),
-        
+        margin: theme.spacing(3),        
     }
   }));
 
 export default function DeveloperBoard() {
-    const classes = useStyles();
+    const wideScrenMode = useMediaQuery('(min-aspect-ratio: 8/5)');
+    const classes = useStyles(wideScrenMode);
 
     return (
-        <Box p={4} m={2} className={classes.root} border={2}>
+        <Box p={4} m={2} className={classes.root}>
             <Box className={classes.mainBox} p={4} m={2}>
                 <Box>
                     <Button className={classes.button} variant="contained" color="secondary">1</Button>
